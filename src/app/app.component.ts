4089daf442d1146261c550cb33549e8863750b7c
sheilavazquezch <l17390367@chetumal.tecnm.mx>
2022-06-26 22:51:57 -0500

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejercicioCheckbox';
+  valor1!:number;
+  valor2!:number;
+  resultado!:string;
+  opcion1=false;
+  opcion2=false;
+  opcion3=false;
+  opcion4=false;
+
+  operar() {
+    this.resultado='';
+    if (this.opcion1) {
+      let ope = this.valor1 + this.valor2;
+      this.resultado+=`La suma es ${ope} `;
+    }
+    if (this.opcion2) {
+      let ope = this.valor1 - this.valor2;
+      this.resultado+=`La resta es ${ope} `;
+    }
+    if (this.opcion3) {
+      let ope = this.valor1 * this.valor2;
+      this.resultado+=`El producto es ${ope} `;
+    }
+    if (this.opcion4) {
+      let ope = this.valor1 / this.valor2;
+      this.resultado+=`La division es ${ope} `;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
